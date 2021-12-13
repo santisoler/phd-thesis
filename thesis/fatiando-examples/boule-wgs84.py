6bd7b82e814bf0fcd1f8bcb21d42b7619326ea78
Santiago Soler <santiago.r.soler@gmail.com>
2021-12-13 12:04:28 -0300

    Modificaciones luego del review del primer jurado (#40)
    - Mejora ejemplo de Boule: el ejemplo de Boule hace uso del WGS84 que viene incluido y lo utiliza para calcular gravedad normal y convertir unidades.
    - Extiende párrafo de las conclusiones acerca de las fuentes equivalentes potenciadas por el gradiente.
    - Reemplaza fuerza centrífuga por aceleración centrífuga.
    - Estandariza el uso de "solo" sin tilde.
    - Correcciones ortográficas y gramaticales en todo el texto.
    - Traduce palabras en las tablas de parámetros.
    - Utiliza doble barras verticales para la norma de vectores.
    - Reemplaza "harmónico" por "armónico" en todo el documento.
    - Incluye ecuaciones y listas en párrafos: utiliza caracteres de comentario en lineas vacías para incluir ecuaciones y listas en los párrafos. Elimina los noindent ya que ahora no son necesarios: las oraciones siguientes también son incluidas en los párrafos.
    - Reemplaza "ordenes" por "órdenes".
    - Agrega un nuevo target al Makefile para calcular diferencias con v1.0.0 mediante latexdiff-vc.
    - Remplaza "Densidad" por "Contraste de densidad" en el eje vertical de la figura.
    - Añade tilde en Satélite en figuras.
    - Mejora texto sobre polinomios de Legendre y la evaluación de sus derivadas sobre los nodos de la cuadratura.

--- thesis/fatiando-examples/boule-wgs84.py
+++ thesis/fatiando-examples/boule-wgs84.py
@@ -1,10 +1,20 @@
 import boule
+import verde as vd
 
-# Definimos el elipsoide WGS84
-WGS84 = boule.Ellipsoid(
-    name="WGS84",
-    semimajor_axis=6378137,
-    flattening=1 / 298.257223563,
-    geocentric_grav_const=3986004.418e8,
-    angular_velocity=7292115e-11,
+# Obtenemos el elipsoide WGS84
+elipsoide = boule.WGS84
+
+# Generamos una grilla de puntos de observacion en coordenadas geodesicas
+# ubicados a una altitud geometrica de 150 metros
+region = (-170, 180, -90, 90)
+longitud, latitud, altitud = vd.grid_coordinates(
+    region=region, spacing=10, extra_coords=150
+)
+
+# Calculamos la gravedad sobre estos puntos
+gravedad_normal = elipsoide.normal_gravity(latitud, altitud)
+
+# Convertimos estos puntos a coordenadas esfericas geocentricas
+longitud, latitud_sph, radio = elipsoide.geodetic_to_spherical(
+    longitud, latitud, altitud
 )
